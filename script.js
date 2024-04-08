e4d769d76100dd52b0ea9c2d5f137eb0225338d6
gaby mia <78322726+gabriellemia@users.noreply.github.com>
2024-04-08 14:04:06 +0100

    added an error

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-
 let searchBtn = document.querySelector("#search");
 
-
 async function getWord(newWord) {
-    let url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + newWord;
-    let response = await fetch(url, {
-        headers: {
-        Accept: "application/json",
-    }
-    });
-if (!response.ok) {
+  let url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + newWord;
+  let response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (!response.ok) {
     console.error(response.status);
     console.error(response.text());
     return;
+  }
+  return await response.json();
 }
-return await response.json();
- 
-}
-
 
 let displayMeaning = document.querySelector("#displayMeaning");
 
-
 let displaySynonym = document.querySelector("#displaySynonym");
 
-searchBtn.addEventListener("click", async function() {
-    let newWord = document.querySelector("#word").value;
-    let definition = await getWord(newWord);
-    if (!definition) {
-        displayMeaning.textContent = "That tastes a bit off, please feed me the correct spelling";
-    }
-    displayMeaning.textContent = "💩 " + "Meaning: " + definition[0].meanings[0].definitions[0].definition;
-    displaySynonym.textContent = "💩 " + "Synonym: " + definition[0].meanings[0].synonyms;
+searchBtn.addEventListener("click", async function () {
+  let newWord = document.querySelector("#word").value;
+  let definition = await getWord(newWord);
+  if (!definition) {
+    displayMeaning.textContent = "That tastes a bit off, please feed me the correct spelling";
+  }
+  displayMeaning.textContent = "💩 " + "Meaning: " + definition[0].meanings[0].definitions[0].definition;
+  displaySynonym.textContent = "💩 " + "Synonym: " + definition[0].meanings[0].synonyms;
 });
+
+const button = addEventListener("btn");
